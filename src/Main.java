b739a98984097aa541f1679186155121b8f6b792
Kacper <kurekkacper64@gmail.com>
2025-05-04 21:23:09 +0200

    stworzenie interfejsu uzytkownika

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Calendar data=null;
+        Scanner sc;
+        System.out.println("Please, input data in DD-MM-YYYY format.");
+        sc=new Scanner(System.in);
+        String day=sc.nextLine();
+        sc=new Scanner(System.in);
+        String numOfMonth=sc.nextLine();
+        sc=new Scanner(System.in);
+        int year=sc.nextInt();
+        try{
+            data=new Calendar(day,numOfMonth,year);
+            System.out.println(data.getData());
+        } catch (DayOutOfRangeException e) {
+            System.out.println(e.getMessage());
+        } catch (IndexOutOfBoundsException e) {
+            throw new MonthOutOfRangeException("Month out of range. Your input "+ numOfMonth);
+        }
     }
 }
