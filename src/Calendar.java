fc6b15f4dfe7b2fe92b718c24ed2a0697dc195cb
Kacper <kurekkacper64@gmail.com>
2025-05-05 18:07:07 +0200

    uzupelnienie funckjonalnosci o klase Months

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,27 +1,13 @@
 public class Calendar{
     private String day;
-    private static Month[] months = new Month[]{
-            new Month("01","Styczen",31),
-            new Month("02","Luty",28),
-            new Month("03","Marzec",31),
-            new Month("04","Kwiecien",30),
-            new Month("05","Maj",31),
-            new Month("06","Czerwiec",30),
-            new Month("07","Lipiec",31),
-            new Month("08","Sierpien",31),
-            new Month("09","Wrzesien",30),
-            new Month("10","Pazdziernik",31),
-            new Month("11","Listopad",30),
-            new Month("12","Grudzien",31),
-    };
     private Month month;
     private int year;
     public Calendar(String day, String numOfMonth,int year) throws IndexOutOfBoundsException,DayOutOfRangeException{
         this.year=year;
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
-            months[1].setMaxDayNum(29);
+            Month.months[1].setMaxDayNum(29);
         }
-        month=months[Integer.parseInt(numOfMonth)-1];
+        month=Month.months[Integer.parseInt(numOfMonth)-1];
         if(Integer.parseInt(day)<1 || Integer.parseInt(day)>month.getMaxDayNum()){
             throw new DayOutOfRangeException("Day out of range. Your input: " + day);
         }
@@ -32,16 +18,16 @@ public class Calendar{
     }
     private void handleNewYear(boolean moveBackward){
         if(moveBackward){
-            month = months[11];
+            month = Month.months[11];
             year=year-1;
         } else {
-            month = months[0];
+            month = Month.months[0];
             year=year+1;
         }
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
-            months[1].setMaxDayNum(29);
+            Month.months[1].setMaxDayNum(29);
         } else{
-            months[1].setMaxDayNum(28);
+            Month.months[1].setMaxDayNum(28);
         }
     }
     public void moveByAWeek(boolean moveBackward){
@@ -51,16 +37,16 @@ public class Calendar{
                 handleNewYear(false);
             }
             else {
-                month = months[month.getNumOfMonth()];
+                month = Month.months[month.getNumOfMonth()];
             }
-            day=String.valueOf(Integer.parseInt(day)+interval - months[month.getNumOfMonth()-2].getMaxDayNum());
+            day=String.valueOf(Integer.parseInt(day)+interval - Month.months[month.getNumOfMonth()-2].getMaxDayNum());
             return;
         }
         if(Integer.parseInt(day)+interval < 1){
             if (month.getNumOfMonth() - 1 < 1){
                 handleNewYear(true);
             } else{
-                month = months[month.getNumOfMonth()-2];
+                month = Month.months[month.getNumOfMonth()-2];
             }
             day=String.valueOf(month.getMaxDayNum()+Integer.parseInt(day)+interval);
             return;
